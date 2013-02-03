1f375116d2e0343a84e129060ea72772f73f84ef
Lasse Öörni <loorni@gmail.com>
2013-02-03 19:02:19 +0000

    Use gcommon.h include file for gt2nt2. Fixed subtunes past the first not getting converted.

--- tools/gt2nt2.c
+++ tools/gt2nt2.c
@@ -1,19 +1,10 @@
-#define CMD_DONOTHING 0
-#define CMD_PORTAUP 1
-#define CMD_PORTADOWN 2
-#define CMD_TONEPORTA 3
-#define CMD_VIBRATO 4
-#define CMD_SETAD 5
-#define CMD_SETSR 6
-#define CMD_SETWAVE 7
-#define CMD_SETWAVEPTR 8
-#define CMD_SETPULSEPTR 9
-#define CMD_SETFILTERPTR 10
-#define CMD_SETFILTERCTRL 11
-#define CMD_SETFILTERCUTOFF 12
-#define CMD_SETMASTERVOL 13
-#define CMD_FUNKTEMPO 14
-#define CMD_SETTEMPO 15
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileio.h"
+#include "gcommon.h"
 
 #define MST_NOFINEVIB 0
 #define MST_FINEVIB 1
@@ -25,43 +16,6 @@
 #define PTBL 1
 #define FTBL 2
 #define STBL 3
-
-#define MAX_FILT 64
-#define MAX_STR 32
-#define MAX_INSTR 64
-#define MAX_CHN 3
-#define MAX_PATT 208
-#define MAX_TABLES 4
-#define MAX_TABLELEN 255
-#define MAX_INSTRNAMELEN 16
-#define MAX_PATTROWS 128
-#define MAX_SONGLEN 254
-#define MAX_SONGS 32
-#define MAX_NOTES 96
-
-#define REPEAT 0xd0
-#define TRANSDOWN 0xe0
-#define TRANSUP 0xf0
-#define LOOPSONG 0xff
-
-#define ENDPATT 0xff
-#define INSTRCHG 0x00
-#define FX 0x40
-#define FXONLY 0x50
-#define FIRSTNOTE 0x60
-#define LASTNOTE 0xbc
-#define REST 0xbd
-#define KEYOFF 0xbe
-#define KEYON 0xbf
-#define OLDKEYOFF 0x5e
-#define OLDREST 0x5f
-
-#define WAVEDELAY 0x1
-#define WAVELASTDELAY 0xf
-#define WAVESILENT 0xe0
-#define WAVELASTSILENT 0xef
-#define WAVECMD 0xf0
-#define WAVELASTCMD 0xfe
 
 #define MAX_NTSONGS 16
 #define MAX_NTPATT 127
@@ -79,24 +33,6 @@
 #define NT_LASTNOTE 0x5f*2
 #define NT_DUR 0xc0
 #define NT_MAXDUR 65
-
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "fileio.h"
-
-typedef struct
-{
-  unsigned char ad;
-  unsigned char sr;
-  unsigned char ptr[MAX_TABLES];
-  unsigned char vibdelay;
-  unsigned char gatetimer;
-  unsigned char firstwave;
-  char name[MAX_INSTRNAMELEN];
-} INSTR;
 
 INSTR instr[MAX_INSTR];
 unsigned char ltable[MAX_TABLES][MAX_TABLELEN];
@@ -213,6 +149,7 @@ void loadsong(const char* songfilename)
 
       // Read songorderlists
       amount = fread8(handle);
+      highestusedsong = amount - 1;
       for (d = 0; d < amount; d++)
       {
         for (c = 0; c < MAX_CHN; c++)
@@ -273,6 +210,7 @@ void loadsong(const char* songfilename)
 
       // Read songorderlists
       amount = fread8(handle);
+      highestusedsong = amount - 1;
       for (d = 0; d < amount; d++)
       {
         for (c = 0; c < MAX_CHN; c++)
@@ -282,9 +220,7 @@ void loadsong(const char* songfilename)
           loadsize++;
           fread(songorder[d][c], loadsize, 1, handle);
         }
-        highestusedsong = d;
       }
-
 
       // Read instruments
       amount = fread8(handle);
@@ -368,6 +304,7 @@ void loadsong(const char* songfilename)
 
       // Read songorderlists
       amount = fread8(handle);
+      highestusedsong = amount - 1;
       for (d = 0; d < amount; d++)
       {
         for (c = 0; c < MAX_CHN; c++)
@@ -1057,6 +994,11 @@ void convertsong(void)
 
     memset(slidewavepos, 0, sizeof slidewavepos);
 
+    if (highestusedsong > 15)
+    {
+        printf("More than 16 songs not supported\n");
+        exit(1);
+    }
     if (highestusedpatt > 126)
     {
         printf("More than 127 patterns not supported\n");
