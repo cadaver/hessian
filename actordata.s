b66ee2761b261793d7bd873fed643212ff66dd63
Lasse Öörni <loorni@gmail.com>
2013-02-15 16:10:40 +0000

    Easy mode is easier. Slowed down enemies.

--- actordata.s
+++ actordata.s
@@ -430,12 +430,12 @@ alEnemy:        dc.w MoveAIHuman                ;Update routine
                 dc.b HP_ENEMY                   ;Initial health
                 dc.b 2                          ;Color override
                 dc.b NO_MODIFY                  ;Damage modifier
-                dc.b 20                         ;XP from kill
+                dc.b 5                          ;XP from kill
                 dc.b AIMODE_THUG                ;AI mode when spawned randomly
                 dc.b $07                        ;AI offense accumulator
                 dc.b $08                        ;AI defense probability
                 dc.b AMF_JUMP|AMF_DUCK|AMF_CLIMB|AMF_ROLL|AMF_WALLFLIP ;Move caps
-                dc.b 4*8                        ;Max. movement speed
+                dc.b 28                         ;Max. movement speed
                 dc.b 6*8                        ;Terminal falling speed
                 dc.b 8                          ;Ground movement acceleration
                 dc.b 3                          ;In air movement acceleration
