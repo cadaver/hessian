5153c576c941356003e0ae93cf734e410e2599ee
Lasse Öörni <loorni@gmail.com>
2013-06-13 15:58:57 +0000

    Music instrument fixes. Started new song.

--- music/conv.bat
+++ music/conv.bat
@@ -2,7 +2,9 @@ gt2nt2 title.sng title.nt
 gt2nt2 cargoship.sng cargoship.nt
 gt2nt2 city.sng city.nt
 gt2nt2 wilderness.sng wilderness.nt 56
+gt2nt2 ending.sng ending.nt
 prg2d64 ninjatr2.d64 TITLE title.nt
 prg2d64 ninjatr2.d64 CARGOSHIP cargoship.nt
 prg2d64 ninjatr2.d64 CITY city.nt
 prg2d64 ninjatr2.d64 WILDERNESS wilderness.nt
+prg2d64 ninjatr2.d64 ENDING ending.nt
