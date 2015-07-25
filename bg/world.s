225731ae2741fb23bd951f3614e550b34885604f
Lasse Öörni <loorni@gmail.com>
2015-07-25 20:03:58 +0300

    Brought level 4 in.

--- bg/world.s
+++ bg/world.s
@@ -1,18 +1,20 @@
-NUMLEVELS = 3
+NUMLEVELS = 4
 
-WORLDSIZEBLOCKS = 6100
+WORLDSIZEBLOCKS = 6840
 
-WORLDSIZESCREENS = 136
+WORLDSIZESCREENS = 152
 
-LVLDATAACTTOTALSIZE = 3
+LVLDATAACTTOTALSIZE = 4
 
-LVLOBJTOTALSIZE = 3
+LVLOBJTOTALSIZE = 4
 
 lvlDataActBitsStart:
                 dc.b 0
                 dc.b 1
                 dc.b 2
+                dc.b 3
 lvlDataActBitsLen:
+                dc.b 1
                 dc.b 1
                 dc.b 1
                 dc.b 1
@@ -20,7 +22,9 @@ lvlObjBitsStart:
                 dc.b 0
                 dc.b 1
                 dc.b 2
+                dc.b 3
 lvlObjBitsLen:
+                dc.b 1
                 dc.b 1
                 dc.b 1
                 dc.b 1
@@ -28,15 +32,19 @@ lvlLimitL:
                 dc.b 0
                 dc.b 18
                 dc.b 29
+                dc.b 38
 lvlLimitR:
                 dc.b 18
                 dc.b 31
-                dc.b 48
+                dc.b 41
+                dc.b 51
 lvlLimitU:
                 dc.b 17
                 dc.b 20
                 dc.b 14
+                dc.b 2
 lvlLimitD:
                 dc.b 34
                 dc.b 47
                 dc.b 34
+                dc.b 30
