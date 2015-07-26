42ae9039fdea2d5f7766166b8544cde562a2f37c
Lasse Öörni <loorni@gmail.com>
2015-07-26 03:58:55 +0300

    Larger entrance area. Edits in the security area.

--- bg/world.s
+++ bg/world.s
@@ -1,12 +1,12 @@
 NUMLEVELS = 4
 
-WORLDSIZEBLOCKS = 6840
+WORLDSIZEBLOCKS = 7100
 
-WORLDSIZESCREENS = 152
+WORLDSIZESCREENS = 159
 
 LVLDATAACTTOTALSIZE = 4
 
-LVLOBJTOTALSIZE = 4
+LVLOBJTOTALSIZE = 5
 
 lvlDataActBitsStart:
                 dc.b 0
@@ -22,27 +22,27 @@ lvlObjBitsStart:
                 dc.b 0
                 dc.b 1
                 dc.b 2
-                dc.b 3
+                dc.b 4
 lvlObjBitsLen:
                 dc.b 1
                 dc.b 1
-                dc.b 1
+                dc.b 2
                 dc.b 1
 lvlLimitL:
                 dc.b 0
                 dc.b 18
                 dc.b 29
-                dc.b 38
+                dc.b 40
 lvlLimitR:
                 dc.b 18
-                dc.b 31
-                dc.b 41
+                dc.b 33
+                dc.b 42
                 dc.b 51
 lvlLimitU:
                 dc.b 17
                 dc.b 20
                 dc.b 14
-                dc.b 2
+                dc.b 3
 lvlLimitD:
                 dc.b 34
                 dc.b 47
