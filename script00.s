cdf79db1d63465c32e65dd8516fae30df4a3e800
Lasse Öörni <loorni@gmail.com>
2015-08-16 13:05:22 +0300

    Added trigger objects for testing which open certain doorways that will later be opened by game events.

--- script00.s
+++ script00.s
@@ -21,9 +21,9 @@ logoScreen      = chars+608
 logoColors      = chars+608+168
 titleTexts      = chars+608+168*2
 
-;START_LEVEL     = $00                          ;Warehouse
-;START_X         = $6780
-;START_Y         = $1d00
+START_LEVEL     = $00                          ;Warehouse
+START_X         = $6780
+START_Y         = $1d00
 
 ;START_LEVEL     = $01                          ;Courtyard
 ;START_X         = $3480
@@ -49,9 +49,9 @@ titleTexts      = chars+608+168*2
 ;START_X         = $1e80
 ;START_Y         = $2200
 
-START_LEVEL     = $08                          ;Lower labs
-START_X         = $0580
-START_Y         = $4300
+;START_LEVEL     = $08                          ;Lower labs
+;START_X         = $0580
+;START_Y         = $4300
 
                 org scriptCodeStart
 
