0171bd70bcd66e4b7ca0e0eff351900c5a51136c
Lasse Öörni <loorni@gmail.com>
2016-01-03 02:47:16 +0200

    More adjustments.

--- script01.s
+++ script01.s
@@ -775,15 +775,15 @@ recyclerCostTbl:
                 dc.b 20                         ;Auto rifle
                 dc.b 20                         ;Sniper rifle
                 dc.b 20                         ;Minigun
-                dc.b 20                         ;Flamethrower
+                dc.b 25                         ;Flamethrower
                 dc.b 25                         ;Laser rifle
                 dc.b 30                         ;Plasma gun
-                dc.b 30                         ;EMP generator
-                dc.b 30                         ;Grenade launcher
-                dc.b 35                         ;Bazooka
+                dc.b 25                         ;EMP generator
+                dc.b 25                         ;Grenade launcher
+                dc.b 30                         ;Bazooka
                 dc.b 0                          ;Extinguisher
                 dc.b 25                         ;Grenade
-                dc.b 35                         ;Mine
+                dc.b 30                         ;Mine
                 dc.b 40                         ;Medikit
                 dc.b 40                         ;Battery
                 dc.b 50                         ;Armor
