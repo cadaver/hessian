8d26269bd2b033758cf54d59311a859c3f970c37
Lasse Öörni <loorni@gmail.com>
2016-01-26 01:37:27 +0200

    Added old tunnels lab computer texts. All computer terminal interactions should now be in.

--- script20.s
+++ script20.s
@@ -1,20 +1,23 @@
                 include macros.s
                 include mainsym.s
 
-        ; Script 20, old tunnels give laptop
+        ; Script 20, old tunnels computers + give laptop
 
                 org scriptCodeStart
 
-                dc.w LabComputer
+                dc.w LabComputer4
                 dc.w GiveLaptop2
+                dc.w LabComputer1
+                dc.w LabComputer2
+                dc.w LabComputer3
 
-        ; Lab computer
+        ; Lab computer note #4
         ;
         ; Parameters: -
         ; Returns: -
         ; Modifies: various
 
-LabComputer:    lda #ACT_HACKER
+LabComputer4:   lda #ACT_HACKER
                 jsr FindLevelActor
                 bcc LC_NoActor
                 lda lvlActOrg,y
@@ -35,14 +38,44 @@ LabComputer:    lda #ACT_HACKER
                 stx actScriptF+2
                 lda #$00
                 sta scriptVariable
-LC_NoActor:     jsr SetupTextScreen
-                lda #0
-                sta temp1
-                sta temp2
-                gettext txtLabComputer
+LC_NoActor:     gettext txtNote4
+DisplayCommon:  jsr SetupTextScreen
+                ldy #0
+                sty temp1
+                sty temp2
                 jsr PrintMultipleRows
                 jsr WaitForExit
                 jmp CenterPlayer
+
+        ; Lab computer note #1
+        ;
+        ; Parameters: -
+        ; Returns: -
+        ; Modifies: various
+
+LabComputer1:   lda codes+6*3+2
+                ora #$30
+                sta txtNumber3
+                gettext txtNote1
+                bne DisplayCommon
+
+        ; Lab computer note #2
+        ;
+        ; Parameters: -
+        ; Returns: -
+        ; Modifies: various
+
+LabComputer2:   gettext txtNote2
+                bne DisplayCommon
+
+        ; Lab computer note #3
+        ;
+        ; Parameters: -
+        ; Returns: -
+        ; Modifies: various
+
+LabComputer3:   gettext txtNote3
+                bne DisplayCommon
 
         ; Jeff gives laptop after reading apocalyptic note
         ;
@@ -68,19 +101,61 @@ SEL_Wait:       rts
         ; Messages
 
                      ;0123456789012345678901234567890123456789
-txtLabComputer: dc.b "NOTE #4",0
+txtNote1:       dc.b "NOTE #1",0
+                dc.b " ",0
+                dc.b "SUCCESS: THE AI MATRIX IS STABLE AND",0
+                dc.b "CAPABLE OF COHERENT THOUGHT.",0
+                dc.b " ",0
+                dc.b "TO NOT FORGET, THE THIRD NUMBER FOR THE",0
+                dc.b "SUITE LAB: "
+txtNumber3:     dc.b "X. SIMULATIONS WERE PROMISING",0
+                dc.b "BUT ONLY A LIVE TEST CAN FULLY CONFIRM.",0
+                dc.b "TOO BAD IT'S TOO LATE FOR THE CONTRACT.",0
+                textjump txtNormanSignature
+
+txtNote2:            ;0123456789012345678901234567890123456789
+                dc.b "NOTE #2",0
+                dc.b " ",0
+                dc.b "I HAVE GIVEN THE AI FREE REIGN OF THE",0
+                dc.b "NETHER TUNNEL AND THE INVENTION CHAMBER.",0
+                dc.b "MEANWHILE THIS LAB IS SHIELDED FROM IT",0
+                dc.b "AS A SECURITY PRECAUTION.",0
+                dc.b " ",0
+                dc.b "I GAVE IT A QUESTION TO PONDER: HOW TO",0
+                dc.b "BEST BENEFIT ALL OF MANKIND?",0
+                textjump txtNormanSignature
+
+txtNote3:            ;0123456789012345678901234567890123456789
+                dc.b "NOTE #3",0
+                dc.b " ",0
+                dc.b "SETBACK! THE AI REDEFINED ROBOTS AS THE",0
+                dc.b "'NEW HUMANS' THAT SHALL INHERIT EARTH.",0
+                dc.b "IT HAS BEGUN TO CONSTRUCT SOMETHING, BUT",0
+                dc.b "REFUSES TO EXPLAIN IT TO ME.",0
+                dc.b " ",0
+                dc.b "ADDENDUM: IT'S CALLED 'JORMUNGANDR.' A",0
+                dc.b "HUGE BURROWING MACHINE.",0
+                dc.b " ",0
+                dc.b "LUCKILY THE PLUG CAN BE PULLED AT ANY",0
+                dc.b "MOMENT.",0
+                textjump txtNormanSignature
+
+                     ;0123456789012345678901234567890123456789
+txtNote4:       dc.b "NOTE #4",0
                 dc.b " ",0
                 dc.b "THE AI HAS REPURPOSED THE FIBER-OPTIC",0
                 dc.b "LINK BETWEEN THE SERVER VAULT AND THE",0
                 dc.b "INVENTION CHAMBER.",0
                 dc.b " ",0
                 dc.b "I CALL IT A 'BI-DIRECTIONAL REVENGE",0
-                dc.b "PROTOCOL.' IF COMMUNICATION ON THE LINE",0
-                dc.b "CEASES DUE TO EITHER JORMUNGANDR OR THE",0
-                dc.b "AI BEING DISABLED, THE ONE THAT REMAINS",0
-                dc.b "WILL LAUNCH ITS ATTACK.",0
+                dc.b "PROTOCOL.' IF COMMUNICATION CEASES DUE",0
+                dc.b "TO EITHER JORMUNGANDR OR THE AI BEING",0
+                dc.b "DISABLED, THE ONE THAT REMAINS LAUNCHES",0
+                dc.b "ITS ATTACK.",0
                 dc.b " ",0
-                dc.b "I NEED TO CONTACT RUTGER URGENTLY.",0
+                dc.b "IT'S ALSO TAKEN CONTROL OF THE MILITARY",0
+                dc.b "LINE. I NEED TO CONTACT RUTGER URGENTLY.",0
+txtNormanSignature:
                 dc.b " ",0
                 dc.b "- NORMAN",0,0
 
