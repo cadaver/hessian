66867146622847c8a7798be7556d8da33655c8fe
Lasse Öörni <loorni@gmail.com>
2016-01-28 22:44:17 +0200

    Move the hideout ambush to separate script to shorten the loading pause on first visit (rotordrone boss.)

--- script09.s
+++ script09.s
@@ -11,8 +11,6 @@
                 dc.w Hacker2
                 dc.w Hacker3
                 dc.w Hacker4
-                dc.w HackerAmbush
-                dc.w GiveLaptop
 
         ; Rotor drone boss move routine
         ;
@@ -205,107 +203,6 @@ H4_HasPass:     lda #PLOT_HIDEOUTOPEN           ;Can not return to hideout
                 gettext txtHacker4b
                 jmp H_SpeakCommon
 
-        ; Hacker ambush NPC script
-        ;
-        ; Parameters: -
-        ; Returns: -
-        ; Modifies: various
-
-HackerAmbush:   ldx actIndex
-                ldy #ACTI_PLAYER
-                jsr GetActorDistance
-                lda actF1,x
-                cmp #FR_DIE
-                bcs HA_Dying
-                cmp #FR_DUCK+1
-                beq HA_DieAgain
-                lda actHp,x                     ;Set health (invincible by default)
-                bne HA_HealthSet
-                lda #HP_HACKER
-                sta actHp,x
-HA_HealthSet:   lda #ACT_HIGHWALKER
-                jsr FindActor
-                bcc HA_EnemyDestroyed
-                lda actIndex
-                ldy actHp,x
-                cpy #HP_HIGHWALKER
-                bcs HA_NotDamaged
-                lda #ACTI_PLAYER                ;Attack player once damaged, Jeff otherwise
-HA_NotDamaged:  sta actAITarget,x
-                ldx actIndex
-                lda actXH,x                     ;Continue running if already left
-                cmp #$17
-                bcc HA_Run
-                lda actHp,x
-                cmp #HP_HACKER
-                bcs HA_Wait                     ;Wait until hit once, then run
-HA_Run:         lda #JOY_LEFT
-HA_SetControls: sta actMoveCtrl,x
-                lda #AIMODE_IDLE
-                sta actAIMode,x
-HA_Wait:        rts
-HA_Dying:       lda #DEATH_DISAPPEAR_DELAY      ;Keep resetting the time
-                sta actTime,x
-                lda #ACT_HIGHWALKER
-                jsr FindActor
-                bcs HA_Wait                     ;Wait until enemy gone
-                ldx actIndex
-                lda temp6                       ;Wait until player close
-                cmp #$04
-                bcs HA_Wait
-                lda temp5
-                sta actD,x
-                inc actHp,x
-                lda #FR_DUCK+1
-                sta actF1,x
-                sta actF2,x
-                lda #JOY_DOWN
-                jsr HA_SetControls
-                ldy #ACT_HACKER
-                gettext txtAmbushDeath  
-                jmp SpeakLine
-HA_DieAgain:    lda #FR_DIE+2
-                sta actF1,x
-                sta actF2,x
-                dec actHp,x
-HA_StopScript:  lda #$00                        ;Stop actor script exec
-                sta actScriptF+2
-                rts
-HA_EnemyDestroyed:
-                lda #PLOT_HIDEOUTAMBUSH
-                jsr ClearPlotBit
-                ldx actIndex
-                lda #HP_NONCOMBATANT
-                sta actHp,x                     ;Make sure to not allow damage now
-                lda #AIMODE_TURNTO
-                sta actAIMode,x
-                lda temp6                       ;Wait until close
-                cmp #$02
-                bcs HA_Wait
-                jsr AddQuestScore
-                lda #<EP_GIVELAPTOP
-                sta actScriptEP+2
-                gettext txtAmbushVictory
-                jmp H_SpeakCommon
-
-        ; Give laptop script (end of ambush)
-        ;
-        ; Parameters: -
-        ; Returns: -
-        ; Modifies: various
-
-GiveLaptop:     lda #$00
-                sta actScriptF+2                ;Stop script exec now
-                lda #PLOT_HIDEOUTOPEN
-                jsr ClearPlotBit                ;Hideout will be closed from now on
-                lda #SFX_PICKUP
-                jsr PlaySfx
-                lda #ITEM_LAPTOP
-                ldx #1
-                jsr AddItem
-                gettext txtGiveLaptop
-                jmp H_SpeakCommon
-
         ; Messages
 
 txtHacker1:     dc.b 34,"HEY. YOU MUST BE KIM. THE SCIENTISTS TOLD YOU MIGHT DROP BY. "
@@ -327,15 +224,5 @@ txtHacker4a:    dc.b 34,"BUT GO AND TAKE CARE OF THOSE SCIENTISTS NOW. THEY'RE M
 
 txtHacker4b:    dc.b 34,"YOU'VE GOT THE OLD TUNNELS PASS? I THINK WE SHOULD HEAD THERE IMMEDIATELY. "
                 dc.b "I'LL LOCK THE HIDEOUT, SO USE THE RECYCLER NOW IF YOU NEED.",34,0
-
-txtAmbushDeath: dc.b 34,"SUCKS IT HAPPENED LIKE THIS. BUT BETTER WITH YOU HERE. JUST.. PROMISE TO KICK THEIR ASS.",34,0
-
-txtAmbushVictory:
-                dc.b 34,"THEY JAMMED THE RADIO AND FOOLED THE DOOR CAMERA TO GET IN. ONE MORE SECOND AND.. "
-                dc.b "I'D HUG YOU, BUT THOSE GUNS ARE IN THE WAY. WILL SET A HARD LOCK-DOWN NOW, "
-                dc.b "SO USE THE RECYCLER IF YOU NEED.",0
-
-txtGiveLaptop:  dc.b "ALSO TAKE THIS LAPTOP. IF YOU CAN FIND THE DEDICATED LINK, I'D LIKE TO ANALYZE THE TRAFFIC, TO "
-                dc.b "SEE IF WE CAN JUST SAFELY CUT THE AI'S ACCESS.",34,0
 
                 checkscriptend
--- script16.s
+++ script16.s
@@ -1,13 +1,15 @@
                 include macros.s
                 include mainsym.s
 
-        ; Script 16, throne chief
+        ; Script 16, throne chief + hideout ambush
 
                 org scriptCodeStart
 
                 dc.w ThroneChief
                 dc.w BeginAmbush
                 dc.w RadioConstruct2
+                dc.w HackerAmbush
+                dc.w GiveLaptop
 
         ; Throne chief corpse
         ;
@@ -80,6 +82,109 @@ RadioMsg:       ldy #ACT_PLAYER
 BA_Skip:
 RC2_Wait:       rts
 
+
+        ; Hacker ambush NPC script
+        ;
+        ; Parameters: -
+        ; Returns: -
+        ; Modifies: various
+
+HackerAmbush:   ldx actIndex
+                ldy #ACTI_PLAYER
+                jsr GetActorDistance
+                lda actF1,x
+                cmp #FR_DIE
+                bcs HA_Dying
+                cmp #FR_DUCK+1
+                beq HA_DieAgain
+                lda actHp,x                     ;Set health (invincible by default)
+                bne HA_HealthSet
+                lda #HP_HACKER
+                sta actHp,x
+HA_HealthSet:   lda #ACT_HIGHWALKER
+                jsr FindActor
+                bcc HA_EnemyDestroyed
+                lda actIndex
+                ldy actHp,x
+                cpy #HP_HIGHWALKER
+                bcs HA_NotDamaged
+                lda #ACTI_PLAYER                ;Attack player once damaged, Jeff otherwise
+HA_NotDamaged:  sta actAITarget,x
+                ldx actIndex
+                lda actXH,x                     ;Continue running if already left
+                cmp #$17
+                bcc HA_Run
+                lda actHp,x
+                cmp #HP_HACKER
+                bcs HA_Wait                     ;Wait until hit once, then run
+HA_Run:         lda #JOY_LEFT
+HA_SetControls: sta actMoveCtrl,x
+                lda #AIMODE_IDLE
+                sta actAIMode,x
+HA_Wait:        rts
+HA_Dying:       lda #DEATH_DISAPPEAR_DELAY      ;Keep resetting the time
+                sta actTime,x
+                lda #ACT_HIGHWALKER
+                jsr FindActor
+                bcs HA_Wait                     ;Wait until enemy gone
+                ldx actIndex
+                lda temp6                       ;Wait until player close
+                cmp #$04
+                bcs HA_Wait
+                lda temp5
+                sta actD,x
+                inc actHp,x
+                lda #FR_DUCK+1
+                sta actF1,x
+                sta actF2,x
+                lda #JOY_DOWN
+                jsr HA_SetControls
+                ldy #ACT_HACKER
+                gettext txtAmbushDeath  
+                jmp SpeakLine
+HA_DieAgain:    lda #FR_DIE+2
+                sta actF1,x
+                sta actF2,x
+                dec actHp,x
+HA_StopScript:  lda #$00                        ;Stop actor script exec
+                sta actScriptF+2
+                rts
+HA_EnemyDestroyed:
+                lda #PLOT_HIDEOUTAMBUSH
+                jsr ClearPlotBit
+                ldx actIndex
+                lda #HP_NONCOMBATANT
+                sta actHp,x                     ;Make sure to not allow damage now
+                lda #AIMODE_TURNTO
+                sta actAIMode,x
+                lda temp6                       ;Wait until close
+                cmp #$02
+                bcs HA_Wait
+                jsr AddQuestScore
+                lda #<EP_GIVELAPTOP
+                sta actScriptEP+2
+                gettext txtAmbushVictory
+H_SpeakCommon:  ldy #ACT_HACKER
+                jmp SpeakLine
+
+        ; Give laptop script (end of ambush)
+        ;
+        ; Parameters: -
+        ; Returns: -
+        ; Modifies: various
+
+GiveLaptop:     lda #$00
+                sta actScriptF+2                ;Stop script exec now
+                lda #PLOT_HIDEOUTOPEN
+                jsr ClearPlotBit                ;Hideout will be closed from now on
+                lda #SFX_PICKUP
+                jsr PlaySfx
+                lda #ITEM_LAPTOP
+                ldx #1
+                jsr AddItem
+                gettext txtGiveLaptop
+                jmp H_SpeakCommon
+
         ; Messages
 
 txtRadioConstruct2:
@@ -108,5 +213,15 @@ txtThroneChief:      ;0123456789012345678901234567890123456789
                 dc.b "BLEED TO DEATH; CONSIDER IT ATONEMENT.",0
                 dc.b " ",0
                 dc.b "- NORMAN THRONE",0,0
+
+txtAmbushDeath: dc.b 34,"SUCKS IT HAPPENED LIKE THIS. BUT BETTER WITH YOU HERE. JUST.. PROMISE TO KICK THEIR ASS.",34,0
+
+txtAmbushVictory:
+                dc.b 34,"THEY JAMMED THE RADIO AND FOOLED THE DOOR CAMERA TO GET IN. ONE MORE SECOND AND.. "
+                dc.b "I'D HUG YOU, BUT THOSE GUNS ARE IN THE WAY. WILL SET A HARD LOCK-DOWN NOW, "
+                dc.b "SO USE THE RECYCLER IF YOU NEED.",0
+
+txtGiveLaptop:  dc.b "ALSO TAKE THIS LAPTOP. IF YOU CAN FIND THE DEDICATED LINK, I'D LIKE TO ANALYZE THE TRAFFIC, TO "
+                dc.b "SEE IF WE CAN JUST SAFELY CUT THE AI'S ACCESS.",34,0
 
                 checkscriptend
