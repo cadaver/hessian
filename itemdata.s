9e25a4bfef0025bada57c967c5ba3ac55f8df9f7
Lasse Öörni <loorni@gmail.com>
2016-02-08 22:17:17 +0200

    Reduce amount of shotgun ammo in the Entrance area so that raiding the security center has more of a significance. Reduce pistol drop from enemies to 5 bullets as it's also easy to max out, and to make melee weapons more attractive early on.

--- itemdata.s
+++ itemdata.s
@@ -48,7 +48,7 @@ itemDefaultPickup:
                 dc.b 1                          ;Knife
                 dc.b 1                          ;Nightstick
                 dc.b 1                          ;Bat
-                dc.b 6                          ;Pistol
+                dc.b 5                          ;Pistol
                 dc.b 4                          ;Shotgun
                 dc.b 10                         ;Auto rifle
                 dc.b 3                          ;Sniper rifle
