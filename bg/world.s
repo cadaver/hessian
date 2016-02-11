3f14fbcfd2d42bf196c4907c84912d8266cb73a1
Lasse Öörni <loorni@gmail.com>
2016-02-11 20:12:40 +0200

    Remove one tank enemy from the garage.

--- bg/world.s
+++ bg/world.s
@@ -4,46 +4,46 @@ WORLDSIZEBLOCKS = 32190
 
 WORLDSIZESCREENS = 704
 
-LVLDATAACTTOTALSIZE = 71
+LVLDATAACTTOTALSIZE = 70
 
 LVLOBJTOTALSIZE = 28
 
 lvlDataActBitsStart:
                 dc.b 0
                 dc.b 5
-                dc.b 9
-                dc.b 11
-                dc.b 16
-                dc.b 20
-                dc.b 23
-                dc.b 29
-                dc.b 32
-                dc.b 41
-                dc.b 43
-                dc.b 50
-                dc.b 56
-                dc.b 59
-                dc.b 64
-                dc.b 68
+                dc.b 8
+                dc.b 10
+                dc.b 15
+                dc.b 19
+                dc.b 22
+                dc.b 28
+                dc.b 31
+                dc.b 40
+                dc.b 42
+                dc.b 49
+                dc.b 55
+                dc.b 58
+                dc.b 63
+                dc.b 67
 lvlObjBitsStart:
+                dc.b 70
                 dc.b 71
                 dc.b 72
                 dc.b 73
-                dc.b 74
-                dc.b 76
-                dc.b 78
-                dc.b 80
+                dc.b 75
+                dc.b 77
+                dc.b 79
+                dc.b 82
                 dc.b 83
-                dc.b 84
+                dc.b 86
                 dc.b 87
-                dc.b 88
+                dc.b 90
                 dc.b 91
                 dc.b 92
-                dc.b 93
+                dc.b 95
                 dc.b 96
-                dc.b 97
 lvlObjBitsEnd:
-                dc.b 99
+                dc.b 98
 lvlLimitL:
                 dc.b 0
                 dc.b 18
