722c5a64dad5ccf0852fada1b4ff0313a90bcee9
Lasse Öörni <loorni@gmail.com>
2018-02-11 23:13:04 +0200

    Revert EMP drop amount to 1.

--- itemdata.s
+++ itemdata.s
@@ -56,7 +56,7 @@ itemDefaultPickup:
                 dc.b 30                         ;Flamethrower
                 dc.b 10                         ;Laser rifle
                 dc.b 7                          ;Plasma gun
-                dc.b 2                          ;EMP generator
+                dc.b 1                          ;EMP generator
                 dc.b 3                          ;Grenade launcher
                 dc.b 2                          ;Bazooka
                 dc.b 100                        ;Extinguisher
